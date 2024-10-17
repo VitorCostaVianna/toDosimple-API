a8f915d95267a303805082a893e1ce1b0b333f93
VitorCostaVianna <vcvianna@sga.pucminas.br>
2024-10-17 17:54:03 -0300

    Create Taskk model

--- src/main/java/com/vitorvianna/todosimple/models/User.java
+++ src/main/java/com/vitorvianna/todosimple/models/User.java
@@ -1,18 +1,19 @@
 package com.vitorvianna.todosimple.models;
 
-
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity // entidade no banco de dados ou seja tabela
 @Table(name = User.TABLE_NAME)
@@ -45,7 +46,8 @@ public class User {
       max = 60)
   private String password;
 
-  // private List<Task> tasks = new ArrayList<Task>();
+  @OneToMany(mappedBy = "user")
+  private List<Task> tasks = new ArrayList<Task>();
 
   public User() {}
 
@@ -77,6 +79,14 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public List<Task> getTasks() {
+    return tasks;
+  }
+
+  public void setTasks(List<Task> tasks) {
+    this.tasks = tasks;
   }
 
   @Override
